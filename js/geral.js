45f871fe6ff48256ed85039b93cb720226d8c0de
Sitefalcon <164095366+Sitefalcon@users.noreply.github.com>
2024-10-08 20:58:44 -0300

    Add files via upload

--- js/geral.js
+++ js/geral.js
@@ -70,3 +70,25 @@ btnesquerdoquatro.addEventListener('click', function(){
 })
 
 
+// ATIVAÇÃO DO CARROSSEL
+var slideIndex = 0;
+
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+}
+
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "flex";
+  setTimeout(showSlides, 12000); // Change image every 2 seconds
+}
+ 
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
